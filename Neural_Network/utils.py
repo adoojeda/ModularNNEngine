017a5edbc3c5d5eeb7bec23641f47c1208d598f8
JoelClemente <joel.lopez106@alu.ulpgc.es>
2024-10-28 16:23:55 +0000

    Cambios nuevos para corregir

--- Neural_Network/utils.py
+++ Neural_Network/utils.py
@@ -1,15 +1,5 @@
 # utils.py
 import numpy as np
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-
-def preprocess_data(X, y):
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-
-    encoder = OneHotEncoder(sparse_output=False)
-    y_encoded = encoder.fit_transform(y)
-
-    return X_scaled, y_encoded
 
 def train_test_split(X, y, test_size=0.2, random_seed=42):
         
