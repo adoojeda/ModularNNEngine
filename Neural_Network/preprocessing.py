86b441dfa81b3d1246120121974fa9e39acb7cef
JoelClemente <joel.lopez106@alu.ulpgc.es>
2024-10-28 19:50:18 +0000

    Se añade el intento de hacerlo con MNIST y se comenta el test

--- Neural_Network/preprocessing.py
+++ Neural_Network/preprocessing.py
@@ -1,7 +1,7 @@
 #prepocessing.py
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
-def preprocess_data(X, y):
+def preprocess_datairis(X, y):
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
 
@@ -9,3 +9,9 @@ def preprocess_data(X, y):
     y_encoded = encoder.fit_transform(y)
 
     return X_scaled, y_encoded
+
+def preprocess_datamnist(X,y):
+    encoder = OneHotEncoder(sparse_output=False)
+    y_encoded = encoder.fit_transform(y.reshape(-1, 1))  
+
+    return X, y_encoded
